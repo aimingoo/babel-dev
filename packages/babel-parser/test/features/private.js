5da5aabdb742891662cf508d97a4f32b647d3f57
aimingoo <aiming@gmail.com>
2019-08-21 18:58:04 +0800

    more testcases

--- packages/babel-parser/test/features/private.js
+++ packages/babel-parser/test/features/private.js
@@ -1,15 +1,17 @@
 class MyClass {
+  // private x, y, z = 100;
   private x;
   private x = 100;
   private x as y;
   private x as y = 200;
   private as y;
   private as y = 100;
-  private as y = 100;
 
   // static, async, get/setter, methods, and more
   private static x;
   private async x() {};
+  private *x() {};
+  private async *x() {};
   private get x() {};
   private x() {};
 
@@ -17,6 +19,14 @@ class MyClass {
   public x;
   public as x;
   protected x = 100;
+
+  // computed name or value, for all accessibilities
+  private [a+b] = 100; // IdentiferName is result of a+b
+  private a = a+b; // value is a+b
+
+  // normal style
+  foo() {}
+  static foo() {}
 }
 
 obj = {
